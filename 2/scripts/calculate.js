9564c6e59aff1d349c6b32d0084601049a972a5c
John-John Markstedt <John-John@Markstedts.org>
2019-05-02 11:52:07 +0200

    Update calculate.js

--- 2/scripts/calculate.js
+++ 2/scripts/calculate.js
@@ -1,20 +1,20 @@
-// Ugly af, add to markup instead. Assume here as intro to modify DOM and to avoid to grade multiple files?
+// Ugly, add to markup instead. Assume only here to avoid grading multiple files?
 document.getElementById("content").innerHTML +=
     "<input type='button' class='btn btn-primary' id='calculate' value='calculate'/>";
 
-// Add column to table
+// Add column to table, add to markup instead.
 document.getElementsByClassName("thead-dark")[0].children[0].innerHTML +=
     "<th>Summa</th>";
 
-// Add summation row
+// Add summation row, add to markup instead.
 document.getElementById("pricetable").children[1].innerHTML +=
     " <tr>\n" +
-        "<td>Summation</td>\n" +
-        "<td> - </td>\n" +
-        "<td> - </td>\n" +
-        "<td> - </td>\n" +
-        "<td> - </td>\n" +
-        "<td> - </td>\n" +
+    "<td>Summation</td>\n" +
+    "<td> - </td>\n" +
+    "<td> - </td>\n" +
+    "<td> - </td>\n" +
+    "<td> - </td>\n" +
+    "<td> - </td>\n" +
     "</tr>";
 
 // Calculates sum of each product.
@@ -23,28 +23,28 @@ document.getElementById("calculate").addEventListener("click",(()=>{
     let items = 0;
     let total = 0;
 
+    // Re-calculate the summation.
     [...document.getElementById("pricetable").children[1].children].forEach((row, i, arr)=>{
         if (i === arr.length - 1){
             return;
         }
-            row.children[5] ?
+        row.children[5] ?
             row.children[5].innerHTML = "<td>"+row.children[3].innerHTML*row.children[4].children[0].value+"</td>"
             :
-            row.innerHTML += "<td>"+row.children[3].innerHTML*row.children[4].children[0].value+"</td>"
+            row.innerHTML += "<td>"+row.children[3].innerHTML*row.children[4].children[0].value+"</td>";
 
         total += parseInt(row.children[3].innerHTML)* parseInt(row.children[4].children[0].value);
         items += parseInt(row.children[4].children[0].value);
     });
 
-
-    let table = document.getElementById("pricetable").children[1].children
+    let table = document.getElementById("pricetable").children[1].children;
     table[table.length-1].innerHTML =
         " <tr>\n" +
-            "<td>Summation</td>\n" +
-            "<td> - </td>\n" +
-            "<td> - </td>\n" +
-            "<td> - </td>\n" +
-            "<td>"+items+"</td>\n" +
-            "<td>"+total+"</td>\n" +
+        "<td>Summation</td>\n" +
+        "<td> - </td>\n" +
+        "<td> - </td>\n" +
+        "<td> - </td>\n" +
+        "<td>"+items+"</td>\n" +
+        "<td>"+total+"</td>\n" +
         "</tr>";
 }));
